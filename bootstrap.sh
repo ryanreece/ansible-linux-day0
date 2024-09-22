ac05a7d9b85b34e45af3a576b6b6311c8b7216f8
Ryan Reece <ryan@reece.ai>
2024-09-21 22:32:14 -0700

    fix: add --yes to apt install and use become for luarocks

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,7 +21,7 @@ case $ID in
         sudo apt update
         sudo apt install software-properties-common
         sudo add-apt-repository --yes --update ppa:ansible/ansible
-        sudo apt install ansible
+        sudo apt install --yes ansible
         ;;
     rocky)
         sudo dnf install ansible
