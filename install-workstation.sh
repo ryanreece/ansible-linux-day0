956b7b330ac8d41fd558c482675c1ada6f80e5b2
Ryan Reece <ryan@reece.ai>
2024-09-22 12:36:08 -0700

    chore: add more notes to install-workstation.sh

--- install-workstation.sh
+++ install-workstation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+echo -e "\nInitiating install-workstation.sh!"
+echo -e "\nSystem will ask for BECOME password. This is your sudo password!\n"
+
 ansible-playbook -i localhost, day0-workstation.yml --connection=local -K
+
+echo "Ansible playbook day0-workstation.yml complete!"
+echo -e "\n\nSystem will reboot in 30s..."
+echo "Press Ctrl+c to abort!"
+
+sleep 30s
+
+sudo reboot
